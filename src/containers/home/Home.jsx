6b225b1c0a67e3836d5a462b7eb29027f8fa2b31
Rafa <rafa.linares.molina@gmail.com>
2020-10-24 11:11:34 +0200

    Updated style of Home

--- src/containers/home/Home.jsx
+++ src/containers/home/Home.jsx
@@ -3,27 +3,24 @@ import { BrowserRouter, Switch, Route, Link, Redirect } from "react-router-dom";
 import Login from "../login/Login";
 import Register from "../register/Register";
 import HomeInfo from "./HomeInfo";
-import "./Home.scss"
-function Home({user,setUser}) {
+import "./Home.scss";
+
+import Header from "../header/Header";
+function Home({ user, setUser }) {
   return (
-    <div className="homeContainer">
-      <div className="buttons">
-            <Link to="/login">To Login</Link>
-            <Link to="/register">To Register</Link>
-        <BrowserRouter>
-        <Switch>
-          <Route exact path="/" >
-            <HomeInfo />
-          </Route>
-          <Route exact path="/login" >
-            <Login user={user} setUser={setUser}/>
-            </Route>
-          <Route exact path="/register" component={Register} />
-          <Redirect to="/" />
-        </Switch>
-        </BrowserRouter>
-      </div>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <Header />
+          <HomeInfo />
+        </Route>
+        <Route exact path="/login">
+          <Login user={user} setUser={setUser} />
+        </Route>
+        <Route exact path="/register" component={Register} />
+        <Redirect to="/" />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
--- src/containers/home/HomeInfo.jsx
+++ src/containers/home/HomeInfo.jsx
@@ -1,21 +1,74 @@
 import React from "react";
 import './Home.scss'
 
+
+
 function HomeInfo() {
+  const classesClinic = 'homeContent clinic';
+  const classesCovid = 'homeContent covid';
+  const classesEquipment = 'homeContent equipment';
+  const classesPerson = 'homeContent person';
   return (
-    <div className="homeInfo">
-      <h1>Clinica dental </h1>
-      <div className="homeContent"> 
-        <div className="homeImg"> Image </div>
-        <div className="homeText"> TEXT </div> 
+    <div className="homeInfo">      
+      <div className={classesClinic}> 
+        <div className="homeImg">  </div>
+        <div className="homeText"> 
+          <div className="textTitle"> 
+            <h2>Tecnologia ultimo modelo</h2> 
+          </div>
+          <div className="textDesc"> 
+            Lorem ipsum dolor sit amet consectetur
+           adipisicing elit. Magni natus repellat iste perspiciatis 
+           voluptate facilis laudantium animi iusto asperiores tempore 
+           officia nulla, eum cupiditate cumque vitae amet fuga voluptatum 
+           provident.
+          </div>
+        </div> 
       </div>
-      <div className="homeContent"> 
-        <div className="homeText"> TEXT </div> 
-        <div className="homeImg"> Image </div>
+      <div className={classesCovid}> 
+        <div className="homeText"> 
+          <div className="textTitle"> 
+            <h2>Medidas frente al Covid-19</h2> 
+          </div>
+          <div className="textDesc"> 
+            Lorem ipsum dolor sit amet consectetur
+           adipisicing elit. Magni natus repellat iste perspiciatis 
+           voluptate facilis laudantium animi iusto asperiores tempore 
+           officia nulla, eum cupiditate cumque vitae amet fuga voluptatum 
+           provident.
+          </div>
+        </div> 
+        <div className="homeImg">  </div>
       </div>
-      <div className="homeContent"> 
-        <div className="homeImg"> Image </div>
-        <div className="homeText"> TEXT </div> 
+      <div className={classesPerson}> 
+        <div className="homeImg">  </div>
+        <div className="homeText"> 
+          <div className="textTitle"> 
+            <h2>Bonos para revisiones anuales</h2> 
+          </div>
+          <div className="textDesc"> 
+            Lorem ipsum dolor sit amet consectetur
+           adipisicing elit. Magni natus repellat iste perspiciatis 
+           voluptate facilis laudantium animi iusto asperiores tempore 
+           officia nulla, eum cupiditate cumque vitae amet fuga voluptatum 
+           provident.
+          </div>
+        </div> 
+      </div>
+      <div className={classesEquipment}> 
+        <div className="homeText">
+          <div className="textTitle"> 
+            <h2>Profesionales dedicados</h2> 
+          </div>
+          <div className="textDesc"> 
+            Lorem ipsum dolor sit amet consectetur
+           adipisicing elit. Magni natus repellat iste perspiciatis 
+           voluptate facilis laudantium animi iusto asperiores tempore 
+           officia nulla, eum cupiditate cumque vitae amet fuga voluptatum 
+           provident.
+          </div>  
+        </div> 
+        <div className="homeImg">  </div>
       </div>
     </div>
   );
