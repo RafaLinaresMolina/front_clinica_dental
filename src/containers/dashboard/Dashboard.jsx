cf936f5050d9f7ac6bbc18c1652207f380f208f3
Rafa <rafa.linares.molina@gmail.com>
2020-10-24 20:57:08 +0200

    updated dashboard

--- src/containers/dashboard/Dashboard.jsx
+++ src/containers/dashboard/Dashboard.jsx
@@ -1,23 +1,27 @@
-import React from "react"
+import React from "react";
 import Admin from "../admin/Admin";
 import Client from "../client/Client";
 import Dentist from "../dentist/Dentist";
 
 function Dashboard(props) {
-
   const cargarVista = (roleId) => {
+    console.log(roleId);
     const layouts = {
-      0: <Admin user={props.user}/>,
-      1: <Client user={props.user}/>,
-      2: <Dentist user={props.user}/>
-    }
-    return layouts[roleId] ? layouts[roleId] : <Client user={props.user}/>;
-  }
+      0: <Admin user={props.user} setUser={props.setUser} />,
+      1: <Client user={props.user} setUser={props.setUser} />,
+      2: <Dentist user={props.user} setUser={props.setUser} />,
+    };
+    return layouts[roleId] ? (
+      layouts[roleId]
+    ) : (
+      <Client user={props.user} setUser={props.setUser} />
+    );
+  };
 
   return (
     <div className="homeInfo">
+      {console.log(JSON.stringify(props, null, 2))}
       {cargarVista(props.user.roleId)}
-      <div>! {props.user.name} !</div>
     </div>
   );
 }
--- src/containers/home/Home.jsx
+++ src/containers/home/Home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Switch, Route, Link, Redirect } from "react-router-dom";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 import Login from "../login/Login";
 import Register from "../register/Register";
 import HomeInfo from "./HomeInfo";
