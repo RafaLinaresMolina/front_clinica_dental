0fddebc6c6fd055fa1afd4aa3e9327aa72375ed3
Rafa <rafa.linares.molina@gmail.com>
2020-10-25 16:26:03 +0100

    removed unused imports

--- src/containers/client/Client.jsx
+++ src/containers/client/Client.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import ClientContent from "./components/ClientContent";
 import ClientNavBar from "./components/ClientNavBar";
 import './Client.scss';
